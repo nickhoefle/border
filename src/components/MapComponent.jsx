be3702f6c06caebe2d1a33db183f1043615bf159
Nick Hoefle <Nickhoefle@gmail.com>
2024-01-29 22:17:33 -0600

    Read in actual FY07-23 data; changed country outlines; removed Antarctica; displaying total on country hover.

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,32 +1,32 @@
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import React, { useEffect, useCallback, useMemo } from 'react';
+import { MapContainer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import jsonData from '../data/FY21-23.json';
+import encountersByCountryData from '../data/FY07-23.json';
 
 const MapComponent = () => {
-    const geoJSONData = require('../world.geo.json');
+    const worldMapGeoJSONData = require('../world.geo.json');
 
-    geoJSONData.features.forEach((feature) => {
-        feature.properties.customValue = Math.random() * 100; // Replace this with actual data
-    });
-
-    const { countriesSet, encounterCountByCountry } = useMemo(() => {
+    const { uniqueCountries, encounterencountersByCountry } = useMemo(() => {
         const set = new Set();
-        const countByCountry = {};
+        const encountersByCountry = {};
 
-        jsonData.forEach((row) => {
+        encountersByCountryData.forEach((row) => {
             const citizenship = row.Citizenship;
-
             if (!set.has(citizenship)) {
                 set.add(citizenship);
-                countByCountry[citizenship] = 0; 
+                encountersByCountry[citizenship] = 0; 
             }
-
-            countByCountry[citizenship] += row['Encounter Count'];
+            encountersByCountry[citizenship] += row['Encounter Count'];
         });
 
-        return { countriesSet: set, encounterCountByCountry: countByCountry };
+        return { uniqueCountries: set, encounterencountersByCountry: encountersByCountry };
     }, []);
+
+    worldMapGeoJSONData.features.forEach((feature) => {
+        if (uniqueCountries.has(feature.properties.name.toUpperCase())) {
+            feature.properties.customValue = encounterencountersByCountry[feature.properties.name.toUpperCase()];
+        }
+    });
 
     const handleMouseOver = useCallback((event) => {
         const layer = event.target;
@@ -34,7 +34,6 @@ const MapComponent = () => {
             fillOpacity: 1,
         });
 
-        // Display the tooltip on hover
         layer.bindTooltip(`<strong>${layer.feature.properties.name}</strong><br />Custom Value: ${layer.feature.properties.customValue}`).openTooltip();
     }, []);
 
@@ -44,17 +43,13 @@ const MapComponent = () => {
             fillOpacity: 0.7,
         });
 
-        // Close the tooltip on mouseout
         layer.closeTooltip();
     }, []);
 
     useEffect(() => {
-        // Log the Set of unique countries
-        console.log([countriesSet]);
-
-        // Log the object with "Encounter Count" for each country
-        console.log(encounterCountByCountry);
-    }, [countriesSet, encounterCountByCountry, handleMouseOver, handleMouseOut]);
+        console.log([uniqueCountries]);
+        console.log(encounterencountersByCountry);
+    }, [uniqueCountries, encounterencountersByCountry, handleMouseOver, handleMouseOut]);
 
     return (
         <MapContainer
@@ -62,18 +57,15 @@ const MapComponent = () => {
             zoom={2}
             style={{ height: '500px', width: '100%' }}
         >
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
+`1`
             <GeoJSON
-                data={geoJSONData}
+                data={worldMapGeoJSONData}
                 style={(feature) => ({
                     fillColor: `rgba(0, 128, 0, ${feature.properties.customValue / 100})`,
-                    weight: 2,
+                    weight: 1,
                     opacity: 1,
-                    color: 'white',
-                    dashArray: '3',
+                    color: 'black',
+                    dashArray: '0',
                     fillOpacity: 0.7,
                 })}
                 onEachFeature={(feature, layer) => {
