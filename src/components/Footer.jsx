d8617bede4970a681af694e63ab2caa5959acd78
Nick Hoefle <Nickhoefle@gmail.com>
2024-02-10 22:18:20 -0600

    Added React formatting to Footer.jsx; added CBP.GOV <a>.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,26 @@ import React  from 'react';
 const Footer = () => {
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '18px' }}>
+        <div id='footerWrapper'>
             <a href='https://www.github.com/nickhoefle'>
-                <img src={'/icons/github-mark.png'} height="40px"/>
+                <img 
+                    src={'/icons/github-mark.png'} 
+                    height='40px'
+                    alt='github-icon'
+                />
             </a>
-            <a href="/jsondata">JSON</a>
+            <a 
+                className='footerLink'
+                href='/jsondata'
+            >
+                JSON
+            </a>
+            <a 
+                className='footerLink'
+                href='https://www.cbp.gov/document/stats/nationwide-encounters'
+            >
+                CBP.GOV
+            </a>
         </div>
     );
 };
